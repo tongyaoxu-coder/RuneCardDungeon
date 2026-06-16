40a2c19108b04c65c3c572fdf22a0e725bc1d495
zzzlzh12-lgtm <zzzlzh12@gmail.com>
2026-06-16 19:03:10 +0800

    GameManager.java

--- src/main/java/com/runecarddungeon/app/GameManager.java
+++ src/main/java/com/runecarddungeon/app/GameManager.java
@@ -1,1 +1,24 @@
-hello
+package app;
+
+import battle.BattleManager;
+import model.Card;
+
+public class GameManager {
+
+    private BattleManager battle;
+
+    public GameManager(BattleManager battle) {
+        this.battle = battle;
+    }
+
+    public void playCard(Card card) {
+
+        battle.getEnemy().takeDamage(
+                card.getDamage()
+        );
+
+        System.out.println(
+                "使用卡牌：" + card.getName()
+        );
+    }
+}
