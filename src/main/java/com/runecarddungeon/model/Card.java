3bb67ff654985a91776bf1ced596559e27d2f3d9
zzzlzh12-lgtm <zzzlzh12@gmail.com>
2026-06-16 19:02:16 +0800

    Card.java

---
+++ src/main/java/com/runecarddungeon/model/Card.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Card {
+
+    private String name;
+    private int damage;
+
+    public Card(String name, int damage) {
+        this.name = name;
+        this.damage = damage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+}
