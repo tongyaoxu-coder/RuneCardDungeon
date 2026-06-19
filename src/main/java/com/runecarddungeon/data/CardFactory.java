889c6a9856a702f6be874e10e56aa567e19594a6
tongyaoxu-coder <tongyaoxu1@gmail.com>
2026-06-19 11:55:01 +0800

    Update CardFactory.java

--- src/main/java/com/runecarddungeon/data/CardFactory.java
+++ src/main/java/com/runecarddungeon/data/CardFactory.java
@@ -1,29 +1,46 @@
 package com.runecarddungeon.data;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.runecarddungeon.effect.BlockEffect;
 import com.runecarddungeon.effect.DamageEffect;
+import com.runecarddungeon.effect.DrawCardEffect;
 import com.runecarddungeon.effect.HealEffect;
 import com.runecarddungeon.model.Card;
+import com.runecarddungeon.model.Deck;
 
-public class CardFactory {
+public final class CardFactory {
 
-    public static List<Card> createStarterDeck() {
-        List<Card> cards = new ArrayList<>();
+    private CardFactory() {
+        // Prevent this utility class from being instantiated.
+    }
 
-        cards.add(new Card("Strike", 1, new DamageEffect(6), "Deal 6 damage."));
-        cards.add(new Card("Strike", 1, new DamageEffect(6), "Deal 6 damage."));
-        cards.add(new Card("Defend", 1, new BlockEffect(5), "Gain 5 block."));
-        cards.add(new Card("Defend", 1, new BlockEffect(5), "Gain 5 block."));
-        cards.add(new Card("Heal", 1, new HealEffect(5), "Restore 5 HP."));
-        cards.add(new Card("Heavy Strike", 2, new DamageEffect(12), "Deal 12 damage."));
-        cards.add(new Card("Iron Wall", 2, new BlockEffect(12), "Gain 12 block."));
-        cards.add(new Card("Greater Heal", 2, new HealEffect(10), "Restore 10 HP."));
-        cards.add(new Card("Power Hit", 3, new DamageEffect(20), "Deal 20 damage."));
-        cards.add(new Card("Guard", 0, new BlockEffect(3), "Gain 3 block."));
+    public static Deck createStarterDeck() {
+        Deck deck = new Deck();
 
-        return cards;
+        deck.addCard(new Card(
+                "Strike",
+                1,
+                new DamageEffect(6),
+                "Deal 6 damage to the enemy."));
+
+        deck.addCard(new Card(
+                "Defend",
+                1,
+                new BlockEffect(5),
+                "Gain 5 block."));
+
+        deck.addCard(new Card(
+                "Healing Rune",
+                2,
+                new HealEffect(6),
+                "Restore 6 HP."));
+
+        deck.addCard(new Card(
+                "Insight",
+                0,
+                new DrawCardEffect(2),
+                "Draw 2 cards."));
+
+        deck.shuffle();
+        return deck;
     }
 }
