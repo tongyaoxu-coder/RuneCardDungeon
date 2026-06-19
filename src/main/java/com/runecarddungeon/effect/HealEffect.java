916b7c6d3a7e9e66a3eadfb6556049114de8e095
tongyaoxu-coder <tongyaoxu1@gmail.com>
2026-06-19 11:49:51 +0800

    Update HealEffect.java

--- src/main/java/com/runecarddungeon/effect/HealEffect.java
+++ src/main/java/com/runecarddungeon/effect/HealEffect.java
@@ -1,16 +1,24 @@
 package com.runecarddungeon.effect;
 
+import com.runecarddungeon.battle.BattleManager;
 import com.runecarddungeon.model.Actor;
 
 public class HealEffect implements CardEffect {
-    private int healAmount;
+
+    private final int healAmount;
 
     public HealEffect(int healAmount) {
         this.healAmount = healAmount;
     }
 
     @Override
-    public void apply(Actor releaser, Actor target) {
-        releaser.heal(healAmount);
+    public void apply(
+            Actor releaser,
+            Actor target,
+            BattleManager battleManager) {
+
+        if (releaser != null) {
+            releaser.heal(healAmount);
+        }
     }
 }
