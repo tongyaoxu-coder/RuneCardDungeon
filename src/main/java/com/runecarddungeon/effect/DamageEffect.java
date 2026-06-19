7396f139f6d201d0e21baee8fe9f549de27683d7
tongyaoxu-coder <tongyaoxu1@gmail.com>
2026-06-19 11:49:13 +0800

    Update DamageEffect.java

--- src/main/java/com/runecarddungeon/effect/DamageEffect.java
+++ src/main/java/com/runecarddungeon/effect/DamageEffect.java
@@ -1,17 +1,24 @@
-
 package com.runecarddungeon.effect;
 
+import com.runecarddungeon.battle.BattleManager;
 import com.runecarddungeon.model.Actor;
 
 public class DamageEffect implements CardEffect {
-    private int damage;
+
+    private final int damage;
 
     public DamageEffect(int damage) {
         this.damage = damage;
     }
 
     @Override
-    public void apply(Actor releaser, Actor target) {
-        target.takeDamage(damage);
+    public void apply(
+            Actor releaser,
+            Actor target,
+            BattleManager battleManager) {
+
+        if (target != null) {
+            target.takeDamage(damage);
+        }
     }
 }
