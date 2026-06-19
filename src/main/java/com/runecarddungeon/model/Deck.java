2bb767186e3ecb46defb40183a259d2fb98b4165
tongyaoxu-coder <tongyaoxu1@gmail.com>
2026-06-19 10:33:32 +0800

    Update Deck.java

--- src/main/java/com/runecarddungeon/model/Deck.java
+++ src/main/java/com/runecarddungeon/model/Deck.java
@@ -4,34 +4,66 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Deck{
-	private List<Card> cards;
-	
-	public Deck() {
-		this.cards= new ArrayList<>();
-	}
-	
-	public void addCard(Card card) {
-		if(card!= null) {
-			this.cards.add(card);
-		}
-	}
-	
-	public void shuffle() {
-		Collections.shuffle(this.cards);
-	}
-	
-	public int getRemainingCount() {
-		return this.cards.size();
-	}
-	
-	public void clear() {
-		this.cards.clear();
-	}
-	
-	//getter, for UI and initializing
-	public List<Card> getCards(){
-		return cards;
-	}
-	
+public class Deck {
+    private List<Card> cards;
+
+    public Deck() {
+        this.cards = new ArrayList<>();
+    }
+
+    public void addCard(Card card) {
+        if (card != null) {
+            this.cards.add(card);
+        }
+    }
+
+    public void addCards(List<Card> cards) {
+        if (cards != null) {
+            this.cards.addAll(cards);
+        }
+    }
+
+    public Card drawCard() {
+        if (cards.isEmpty()) {
+            return null;
+        }
+        return cards.remove(0);
+    }
+
+    public List<Card> drawCards(int amount) {
+        List<Card> drawnCards = new ArrayList<>();
+
+        for (int i = 0; i < amount; i++) {
+            Card card = drawCard();
+            if (card != null) {
+                drawnCards.add(card);
+            }
+        }
+
+        return drawnCards;
+    }
+
+    public void removeCard(Card card) {
+        cards.remove(card);
+    }
+
+    public boolean isEmpty() {
+        return cards.isEmpty();
+    }
+
+    public void shuffle() {
+        Collections.shuffle(this.cards);
+    }
+
+    public int getRemainingCount() {
+        return this.cards.size();
+    }
+
+    public void clear() {
+        this.cards.clear();
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
 }
