f876e28a54f87c589c81b79477f7f4fb09addb14
tongyaoxu-coder <tongyaoxu1@gmail.com>
2026-06-19 11:51:55 +0800

    Update Player.java

--- src/main/java/com/runecarddungeon/model/Player.java
+++ src/main/java/com/runecarddungeon/model/Player.java
@@ -1,21 +1,34 @@
 package com.runecarddungeon.model;
 
 public class Player extends Actor {
-	private int energy;
-	private int maxEnergy;
-	
-	public Player(String name, int maxHp, int maxEnergy) {
-		super(name, maxHp);//inherit from base class
-		this.maxEnergy=maxEnergy;
-		this.energy=maxEnergy;
-	}
-	
-	public void resetEnergy() {
-		this.energy=maxEnergy;
-	}
-	
-	public int getEnergy() {return energy;}
-	//energy cant be negative
-	public void useEnergy(int amount) {this.energy=Math.max(0, this.energy-amount);}
-    
+
+    private int energy;
+    private final int maxEnergy;
+
+    public Player(String name, int maxHp, int maxEnergy) {
+        super(name, maxHp);
+        this.maxEnergy = maxEnergy;
+        this.energy = maxEnergy;
+    }
+
+    public void resetEnergy() {
+        this.energy = maxEnergy;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public int getMaxEnergy() {
+        return maxEnergy;
+    }
+
+    public boolean useEnergy(int amount) {
+        if (amount < 0 || energy < amount) {
+            return false;
+        }
+
+        energy -= amount;
+        return true;
+    }
 }
