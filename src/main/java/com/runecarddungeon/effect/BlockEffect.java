8f04c84f5643d3bee2f017a044f962c85ebcd08e
tongyaoxu-coder <tongyaoxu1@gmail.com>
2026-06-19 11:49:33 +0800

    Update BlockEffect.java

--- src/main/java/com/runecarddungeon/effect/BlockEffect.java
+++ src/main/java/com/runecarddungeon/effect/BlockEffect.java
@@ -1,16 +1,24 @@
 package com.runecarddungeon.effect;
 
+import com.runecarddungeon.battle.BattleManager;
 import com.runecarddungeon.model.Actor;
 
 public class BlockEffect implements CardEffect {
-    private int block;
+
+    private final int block;
 
     public BlockEffect(int block) {
         this.block = block;
     }
 
     @Override
-    public void apply(Actor releaser, Actor target) {
-        releaser.addBlock(block);
+    public void apply(
+            Actor releaser,
+            Actor target,
+            BattleManager battleManager) {
+
+        if (releaser != null) {
+            releaser.addBlock(block);
+        }
     }
 }
