a8dd380eadd4256b534925ce9ba656cd58c9ff9c
zzzlzh12-lgtm <zzzlzh12@gmail.com>
2026-06-22 20:18:19 +0800

    Update Slime.java

--- src/main/java/com/runecarddungeon/model/Slime.java
+++ src/main/java/com/runecarddungeon/model/Slime.java
@@ -2,17 +2,20 @@ package com.runecarddungeon.model;
 
 public class Slime extends Enemy {
 
-    public Slime(String name, int maxHp) {
-        super(name, maxHp, 8);
-    }
-
-    @Override
-    public void rollIntent() {
-        setCurrIntent("Prepare to Attack");
+    public Slime(String name, int maxHp, int attackDamage) {
+        super(name, maxHp, attackDamage);
+        rollIntent();
     }
 
     @Override
     public void takeTurn(Player target) {
+        System.out.println("👾 " + getName() + " Attack: Deals " + getAttackDamage() + " damage！");
         attack(target);
+        rollIntent();
+    }
+
+    @Override
+    public void rollIntent() {
+        this.setCurrIntent("👾 Attack: Deals " + getAttackDamage() + " damage");
     }
 }
