9766d6b8fc0e3c8c65d89e380b76fcbb0c161ebd
zzzlzh12-lgtm <zzzlzh12@gmail.com>
2026-06-22 20:16:32 +0800

    Update Skeleton.java

--- src/main/java/com/runecarddungeon/model/Skeleton.java
+++ src/main/java/com/runecarddungeon/model/Skeleton.java
@@ -1,39 +1,30 @@
-package com.runecarddungeon.model;
+ppackage com.runecarddungeon.model;
 
 public class Skeleton extends Enemy {
-	private int turnCount =0;
-	private int attackPower = 10;
-	private final int shieldAmount=8;
-	
-	public Skeleton() {
-		super("Scare Skeleton",60, 10);
-		rollIntent();
-	}
-	
-	@Override
-	public void takeTurn(Player target) {
-		turnCount++;
-		if(turnCount%3==0) {// 3 behaviors
-			this.attackPower+=4;
-		}else if(turnCount%3==1) {
-			target.takeDamage(this.attackPower);//strike
-		}else {
-			this.addBlock(this.shieldAmount);
-		}
-		rollIntent();
-	}
-	
-	@Override
-	public void rollIntent() {
-		int nextTurn=(turnCount+1)%3;
-		if(nextTurn==0) {
-			this.setCurrIntent("Enraged: Attack +4");
-		}else if(nextTurn==1) {
-			this.setCurrIntent("Strike: hit "+ this.attackPower);
-		}else {
-			this.setCurrIntent("Bone Shield: block "+this.shieldAmount);
-		}
-	}
-	
+    private static final int SHIELD_PER_TURN = 4;
 
+    public Skeleton(String name, int maxHp, int attackDamage) {
+        super(name, maxHp, attackDamage);
+        rollIntent();
+    }
+
+    @Override
+    public void onTurnStart() {
+        // 每回合获得4点护盾
+        addBlock(SHIELD_PER_TURN);
+        System.out.println("🛡️ " + getName() + " Gain  " + SHIELD_PER_TURN + " shield points！");
+    }
+
+    @Override
+    public void takeTurn(Player target) {
+        // 每回合固定攻击
+        System.out.println("⚔️ " + getName() + " Launch an attack, dealing " + getAttackDamage() + " damage！");
+        attack(target);
+        rollIntent();
+    }
+
+    @Override
+    public void rollIntent() {
+        this.setCurrIntent("⚔️ Attack: Deals " + getAttackDamage() + " damage and grants  " + SHIELD_PER_TURN + " shield points");
+    }
 }
