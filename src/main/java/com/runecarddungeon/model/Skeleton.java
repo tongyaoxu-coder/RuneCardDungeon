4a056a249688d00e27af46fed94440519e14504a
zzzlzh12-lgtm <zzzlzh12@gmail.com>
2026-06-22 20:22:01 +0800

    Update Skeleton.java

--- src/main/java/com/runecarddungeon/model/Skeleton.java
+++ src/main/java/com/runecarddungeon/model/Skeleton.java
@@ -12,19 +12,19 @@ public class Skeleton extends Enemy {
     public void onTurnStart() {
         // 每回合获得4点护盾
         addBlock(SHIELD_PER_TURN);
-        System.out.println("🛡️ " + getName() + " Gain  " + SHIELD_PER_TURN + " shield points！");
+        System.out.println(getName() + " Gain  " + SHIELD_PER_TURN + " shield points！");
     }
 
     @Override
     public void takeTurn(Player target) {
         // 每回合固定攻击
-        System.out.println("⚔️ " + getName() + " Launch an attack, dealing " + getAttackDamage() + " damage！");
+        System.out.println(getName() + " Launch an attack, dealing " + getAttackDamage() + " damage！");
         attack(target);
         rollIntent();
     }
 
     @Override
     public void rollIntent() {
-        this.setCurrIntent("⚔️ Attack: Deals " + getAttackDamage() + " damage and grants  " + SHIELD_PER_TURN + " shield points");
+        this.setCurrIntent("Attack: Deals " + getAttackDamage() + " damage and grants  " + SHIELD_PER_TURN + " shield points");
     }
 }
